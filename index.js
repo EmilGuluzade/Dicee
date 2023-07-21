5edee8f1c10b2076806a84b81bd2f353f95bc436
Emil <emilquluzade31@gmail.com>
2023-07-22 00:37:53 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImg1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomImg1);
+
+var randomImg2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomImg2);
+
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Win🚩";
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Win";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
